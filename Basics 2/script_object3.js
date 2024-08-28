699b8e934691d5f1fd078e9707cdf98586511627
deepakchavan18 <68800380+deepakchavan18@users.noreply.github.com>
2024-08-28 15:05:09 +0000

    new

--- Basics 2/script_object3.js
+++ Basics 2/script_object3.js
@@ -1,107 +1,99 @@
-//Singleton
+//Funtions and Parameters
 
-
-// const tinderUser = new Object()    //singleton Object
-
-const tinderUser={}                 ////Non-singleton Object
-
-tinderUser.id="123abc"
-tinderUser.name="Devil"
-tinderUser.isLoggedIn=true
-
-// console.log(tinderUser);
-
-const regularUser = {
-    email:"devil@gmail.com",
-    fullname:{
-        userfullname:{
-            firstname:"Deepak",
-            lastname:"Chavan"
-        }
-    }
-}
- 
-// console.log(regularUser.fullname.userfullname.firstname);
-
-const obj1={ 1:"a", 2:"b" }
-const obj2={ 3:"c", 4:"d" }     
-
-const obj3=obj1+obj2;  // X X X This is not an correct way X X X 
-// console.log(obj3); 
-
-const obj4=Object.assign({} ,obj1,obj2)   //Paraenthesis is optional   so here paranthesis is an traget ans remaining are source
-// console.log(obj4); 
-
-const obj5={...obj1,...obj2}//Spread
-// console.log("Using Spread ",obj5); 
-
-
-
-//Array of Objects {This like of data comes from database}
-
-users =[
-    {
-        id:1,
-        name:"Deepak"
-    },
-    {
-        id:1,
-        name:"Deepak"
-    },
-    {
-        id:1,
-        name:"Deepak"
-    },
-    {
-        id:1,
-        name:"Deepak"
-    }
-]
-
-// console.log(users[1].name);
- 
-// console.log(tinderUser)
-// console.log(Object.keys(tinderUser ));
-// console.log(Object.values(tinderUser ));
-// console.log(Object.entries(tinderUser ));
-
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));   //To chek the Object has it or not (property)
-
-
-
-//------------------------New Concept------------------------
-
-
-const course={
-    coursename:"Javascript",
-    price:"1599",
-    courseInstructor:"hitesh"
+function sayMyName(){
+    console.log("D");
+    console.log("E");
+    console.log("E");
+    console.log("P");
+    console.log("A");
+    console.log("K");
 }
 
 
-// console.log(course.courseInstructor);
-
-// const {courseInstructor} = course
-// console.log(courseInstructor);
-
-        //or
-
-const {courseInstructor:ci} = course
-console.log(ci);
-
-
-// //React(Destructuring)
-
-// const company=({company})=>{
-//     console.log("Hello")
+// sayMyName();
+ 
+// function add(number1,number2){     //()-contains parameters
+//     console.log(number1+number2);
 // }
-// navbar(company="Deepak")
+// add(10,10);    //()- arguments 
 
 
-//ApI is in JSON {JSON is an JS Object notation}
-// {
-//     "name":"Deepak",
-//     "coursename":"JS",
-//     "price":"Free"
-// }
+function add2(number1,number2){     
+    let result = number1+number2;
+    return result;
+    //or
+    // return number1+number2
+    console.log("This will not printed as it comes after return ");
+    
+}
+const result = add2(10,10);    
+
+// console.log("Result : ",result);
+
+
+
+//Types 2 print the function Messege     //console and return
+function loginUserNessage1(username){
+    // console.log(`${username} just logged in !`);          //1
+}
+loginUserNessage1("Deepak")                  //1
+
+
+
+
+function loginUserNessage2(username="Deep"){ //Deep will be default value if name is not passed in the argument
+    if (!username) {                         //(username===undefined) or (!username)
+        console.log("Please enter a Username");
+        return;
+        
+    }
+    return `${username} just logged in !`;                    
+}
+// console.log(loginUserNessage2("Deepak"))        
+
+
+
+
+
+
+//---------------------Functions with Objects--------------------------
+
+
+function calculateCartPrice(val1,val2,...num1){      //(...) - rest opereator
+    return num1
+}
+
+// console.log(calculateCartPrice(500,600,700,800));
+
+
+//how to pass the object to the function and how its used
+const user={
+    user_name:"Deepak",
+    price:199
+}
+
+function Amazon(anyObject){
+    console.log(`Username is ${anyObject.user_name} and Item Price is ${anyObject.price}`);
+}
+// Amazon(user)
+
+Amazon({
+    user_name:"Deep",
+    price:599
+})
+
+
+const myNewArray=[100,200,300,400,500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+ 
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100,200,300,400,500]))
+
+
+
+
+
 
