9b5a003563b6d8a1c32a1bcf30a37397dbf994e7
deepakchavan18 <68800380+deepakchavan18@users.noreply.github.com>
2024-08-30 06:56:27 +0000

    New Changed

--- 02_Basics/script_object1.js
+++ 02_Basics/script_object1.js
@@ -49,3 +49,134 @@ new_object.greetingTwo = function(){
 
 console.log(new_object.greeting())
 console.log(new_object.greetingTwo())
+
+
+
+
+
+
+
+//                         Objects in Java [ Part - 2 ]
+
+
+//Singleton ->when its made using Constructor
+// Object.create
+
+//object literals
+// const JsUser={}
+
+
+// const tinderUser = new Object()    //singleton Object
+
+const tinderUser={}                 ////Non-singleton Object
+
+tinderUser.id="123abc"
+tinderUser.name="Devil"
+tinderUser.isLoggedIn=true
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"devil@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Deepak",
+            lastname:"Chavan"
+        }
+    }
+}
+ 
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={ 1:"a", 2:"b" }
+const obj2={ 3:"c", 4:"d" }     
+
+const obj3=obj1+obj2;  // X X X This is not an correct way X X X 
+// console.log(obj3); 
+
+const obj4=Object.assign({} ,obj1,obj2)   //Paraenthesis is optional   so here paranthesis is an traget ans remaining are source
+// console.log(obj4); 
+
+const obj5={...obj1,...obj2}//Spread
+// console.log("Using Spread ",obj5); 
+
+
+
+//Array of Objects {This like of data comes from database}
+
+users =[
+    {
+        id:1,
+        name:"Deepak"
+    },
+    {
+        id:1,
+        name:"Deepak"
+    },
+    {
+        id:1,
+        name:"Deepak"
+    },
+    {
+        id:1,
+        name:"Deepak"
+    }
+]
+
+// console.log(users[1].name);
+ 
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser ));
+// console.log(Object.values(tinderUser ));
+// console.log(Object.entries(tinderUser ));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));   //To chek the Object has it or not (property)
+
+
+
+
+
+
+
+
+//                         Objects in Java [ Part - 3 ]
+
+
+const course={
+    coursename:"Javascript",
+    price:"1599",
+    courseInstructor:"hitesh"
+}
+
+
+// console.log(course.courseInstructor);
+
+// const {courseInstructor} = course     //to Extract value from object
+// console.log(courseInstructor);
+
+
+
+const {courseInstructor:instructor} = course    // rename 
+// console.log(instructor);
+
+        //or
+
+const {courseInstructor:ci} = course            //rename
+// console.log(ci);
+
+
+// //React(Destructuring)
+
+// const company=({company})=>{
+//     console.log("Hello")
+// }
+// navbar(company="Deepak")
+
+
+//ApI is in JSON {JSON is an JS Object notation}
+// {
+//     "name":"Deepak",
+//     "coursename":"JS",
+//     "price":"Free"
+// }
+
