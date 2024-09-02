cb8e2ebcfaa0e4ff0b913baaa82d0d838b62fa8a
deepakchavan18 <68800380+deepakchavan18@users.noreply.github.com>
2024-09-02 09:08:30 +0000

    New

---
+++ 05_Iterations/script_objects_for_in.js
@@ -0,0 +1,31 @@
+//Itearation for Objects
+
+const myObj={
+    js:"Javascript",
+    cpp:"C++",
+    rb:"Ruby",
+    css:"Cascading Style Sheet"
+}
+
+for (const key in myObj) {
+    // console.log(`Full Form for ${key} is ${myObj[key]}`);
+}
+
+
+
+arr1=["C++","Java","CSS","JavaScript"];
+
+for (const lang in arr1) {
+    // console.log(`Laguages are ${arr1[lang]}`)
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
